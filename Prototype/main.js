f23667dd86572749c200042d3791f080e4c758d1
Tanaf <tanaf.dev@gmail.com>
2020-12-09 11:18:44 +0600

    prototype intro

---
+++ Prototype/main.js
@@ -0,0 +1,63 @@
+function Person(name) {
+    this.name = name
+}
+let p = new Person("Rakib")
+console.log(p)
+
+
+var person = {
+    name: "Rakib",
+    age: 10
+}
+console.log(person)
+
+for (let i in person) {
+    console.log(i)
+}
+console.log(Object.keys(person))
+
+let des = Object.getOwnPropertyDescriptor(person, "name")
+console.log(des)
+
+let baseObj = Object.getPrototypeOf(person)
+console.log(baseObj)
+let dec = Object.getOwnPropertyDescriptor(baseObj, "toString")
+console.log(dec)
+
+Object.defineProperty(person, "name", {
+    enumerable: false,
+    writable: false,
+    configureable: false,
+    value: "S"
+})
+
+function Person(name) {
+    this.name = name
+}
+Person.prototype.PI = 3.1416
+let p1 = new Person("Rakib")
+let p2 = new Person("Smrity")
+console.log(p2)
+console.log(p1)
+console.log(Object.getPrototypeOf(p1))
+console.log(Person.prototype)
+
+function Sq (width) {
+    this.width = width
+    this.getWidth = function() {
+        console.log(`Width is ${this.width}`)
+        // this.draw()
+    }
+}
+
+Sq.prototype = {
+    draw: function() {
+        console.log("Draw")
+        this.getWidth()
+    },
+    toString: function() {
+        return "My width is " + this.width
+    }
+}
+let s1 = new Sq(10)
+let s2 = new Sq(5)
