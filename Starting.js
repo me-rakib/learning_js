60cd29cb60dcab698aed6cc11172a9d372cd333c
Tanaf <tanaf.dev@gmail.com>
2021-04-14 16:31:31 +0600

    till 97

--- Starting.js
+++ Starting.js
@@ -1307,3 +1307,38 @@ interpreted - interpretor run through a program line by line and execute each co
 
 
 //https://www.youtube.com/watch?v=KZ8WpqVXG4Y&list=PL_XxuZqN0xVAu_dWUVFbscqZdTzE8t6Z1&index=93 
+
+// closure is when a function is able to remember and acces it's lexical scope even when that function executing outside of it's lexicul scope
+
+// function A() {
+//     let msg = "I am learning lexical scope and closure."
+
+//     return function() {
+//         console.log(msg)
+//     }
+// }
+
+// let sayHi = A()
+
+// sayHi()
+
+
+// closure and loop
+
+// for(let i=1; i<=5; i++) 
+// {
+//     setTimeout(function() {
+//         console.log(i)
+//     }, 1000*i)
+// }
+
+// for(let i=1; i<=5; i++) {
+//     (function (n) {
+//         setTimeout(function() {
+//             console.log(n)
+//         }, 1000*i)
+//     })(i)
+// }
+
+// https://www.youtube.com/watch?v=QI9rhJ5mYMQ&list=PL_XxuZqN0xVAu_dWUVFbscqZdTzE8t6Z1&index=98
+
