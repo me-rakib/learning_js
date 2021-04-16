0080fa7d54f451477f69125d825e21f4fc76f9de
Tanaf <tanaf.dev@gmail.com>
2021-04-16 16:34:24 +0600

    till 109

---
+++ object_orinted_programming/main.js
@@ -0,0 +1,31 @@
+// const rect = {
+//     width: 100,
+//     height: 50,
+//     draw: function() {
+//         console.log("I'm drawing.")
+//         this.printProperties()
+//     },
+//     printProperties: function() {
+//         console.log(`My width is ${this.width}`)
+//         console.log(`My height is ${this.height}`)
+//         console.log(this)
+//     }
+// }
+
+// rect.draw()
+
+// const abc = {
+//     width: 1,
+//     height: 2,
+//     d: rect.printProperties
+// }
+// abc.d()
+
+// function a() {
+//     console.log(this)
+// }
+// a()
+
+
+// https://www.youtube.com/watch?v=h_h7FDOkl2A&list=PL_XxuZqN0xVAu_dWUVFbscqZdTzE8t6Z1&index=110
+
